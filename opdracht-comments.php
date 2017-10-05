2c554f4512e456c65dbc1c77caf0c9b9581c1d44
Niky <niky.dieben@hotmail.com>
2017-10-05 10:50:14 +0200

    oef 1

---
+++ opdracht-comments.php
@@ -0,0 +1,35 @@
+<?php $naam = "niky dieben"; ?>
+
+
+<!doctype html>
+
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Opdracht comments</title>
+	<link rel="stylesheet" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+	<link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+</head>
+
+<body class="web-backend-opdracht">
+
+	<section class="body">
+
+		<h1>Opdracht comments: deel 1</h1>
+
+		<ul>
+			<li>Maak een pagina (incl. HTML5 doctype), met daarin een PHP-block (inline) dat er voor zorgt dat je voornaam &amp; naam op het scherm tevoorschijn komen.</li>
+
+			<li>Maak een commentaar-block met je naam en e-mail</li>
+		</ul>	
+
+		<p>mijn naam is	<?= $naam ?> !!</p>
+	</section>
+
+</body>
+
+</html>
