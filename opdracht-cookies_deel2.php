2c44641c2f814f27b5e2c639e8629d3ff46cb0ba
Niky <niky.dieben@hotmail.com>
2017-11-21 17:37:16 +0100

    Opdracht Cookies deel 1

---
+++ opdracht-cookies_deel2.php
@@ -0,0 +1,68 @@
+<?php	
+	if ( isset( $_GET[ 'logout' ] ) )
+	{
+
+		setcookie( 'authenticated', '', time() - 1000 );
+		header('location: opdracht-cookies.php');
+	}
+	
+	$fileContent	=	file_get_contents( 'cookiesp1.txt' );
+	$userData		=	explode( ',', $fileContent );
+
+	$message			=	false;
+	$isAuthenticated	=	false;
+
+	if ( !isset( $_COOKIE['authenticated'] ) )
+	{
+		if ( isset( $_POST[ 'submit' ] ) )
+		{
+			if ( $_POST[ 'username' ] == $userData[ 0 ] &&
+					$_POST[ 'password' ] == $userData[ 1 ] )
+			{
+				setcookie( 'authenticated', true, time() + 3600 );
+				header( 'location: opdracht-cookies.php' );
+			}
+			else
+			{
+				$message = 'Gebruikersnaam en/of paswoord niet correct. Probeer opnieuw.';
+			}
+		}
+	}
+	else
+	{
+		$message			=	'U bent ingelogd.';
+		$isAuthenticated	=	true;
+	}
+	?>
+	
+<h1>Inloggen</h1>
+
+	<?php if ($message): ?>
+	<?=	$message ?>
+	<?php endif ?>
+
+		<?php if ( !$isAuthenticated ): ?>
+
+		<form action="opdracht-cookies.php" method="post">
+			<ul>
+				<li>
+					<label for="username">Gebruikersnaam: </label>
+					<input type="text" name="username">
+				</li>
+				<li>
+					<label for="password">Paswoord: </label>
+					<input type="password" name="password">
+				</li>
+				<li>
+					<input type="checkbox" name="checked">
+					<label for="checked">Check mij</label>
+					
+				</li>
+			</ul>
+			<input type="submit" name="submit" value="inloggen">
+		</form>
+		<?php else: ?>
+
+		<a href="opdracht-cookies.php?logout=true">Uitloggen</a>
+
+		<?php endif ?>
